7d3e5d44652631292dff95cff38880de81bc7575
Ibrahim Oguz <ibrahimoguz.ie@gmail.com>
2024-07-28 01:28:29 +0100

    added agedbrie concrete class and unit tests

---
+++ src/GildedRose/AgedBrie.cs
@@ -0,0 +1,28 @@
+﻿namespace GildedRoseKata
+{
+    public class AgedBrie : Item
+    {
+        public AgedBrie()
+        {
+            Name = "Aged Brie";
+        }
+        
+        public void UpdateQuality()
+        {
+            if (Quality < 50)
+            {
+                Quality += 1;
+            }
+
+            SellIn -= 1;
+
+            if (SellIn < 0)
+            {
+                if (Quality < 50)
+                {
+                    Quality += 1;
+                }
+            }
+        }
+    }
+}
---
+++ src/GildedRoseTests/AgedBrieTest.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using GildedRoseKata;
+
+namespace GildedRoseTests
+{
+    public class AgedBrieTest
+    {
+        [Theory]
+        [InlineData(0,5)]
+        [InlineData(4,4)]
+        [InlineData(10,8)]
+        public void QualityIncreaseOverTime(int quality, int sellIn)
+        {
+            var item = new AgedBrie { Quality = quality, SellIn = sellIn };
+            item.UpdateQuality();
+            Assert.Equal(quality + 1, item.Quality);
+        }
+
+        [Theory]
+        [InlineData(50,5)]
+        [InlineData(50,1)]
+        [InlineData(50,0)]
+        public void QualityDoesNotExceedFifty(int quality, int sellIn)
+        {
+            var item = new AgedBrie { Quality = quality, SellIn = sellIn };
+            item.UpdateQuality();
+            Assert.Equal(50, item.Quality);
+        }
+
+        [Theory]
+        [InlineData(5,0)]
+        [InlineData(10,-1)]
+        [InlineData(20,-10)]
+        public void QualityIncreaseTwiceAfterSellIn(int quality, int sellIn)
+        {
+            var item = new AgedBrie { Quality = quality, SellIn = sellIn };
+            item.UpdateQuality();
+            Assert.Equal(quality + 2, item.Quality);
+        }
+    }
+}
